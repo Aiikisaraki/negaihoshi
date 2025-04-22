c168a85ea6357e05da28c012faff81636e341e70
Aii 如樱如月 <morikawa@kimisui56.work>
2025-04-22 22:00:07 +0800

    add frontend part

---
+++ frontend/aii-home/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='aii-header'>
+        
+      </div>
+      <div className='aii-content'>
+        
+      </div>
+      <div className='aii-footer'>
+        <p>Copyright © 2025 Aiikisaraki</p>
+        <p>All rights reserved</p>
+      </div>
+    </>
+  )
+}
+
+export default App
--- server/main.go
+++ server/main.go
@@ -2,7 +2,7 @@
  * @Author: Aii 如樱如月 morikawa@kimisui56.work
  * @Date: 2025-04-22 14:52:22
  * @LastEditors: Aii 如樱如月 morikawa@kimisui56.work
- * @LastEditTime: 2025-04-22 17:30:45
+ * @LastEditTime: 2025-04-22 17:37:54
  * @FilePath: \nekaihoshi\server\main.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
